8f5f44a7c565d6408f32f7536de3b0dc69b546ed
Rullian Siqueira da Mata <rullianjbt@gmail.com>
2021-03-07 19:02:21 -0300

    Department entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -5,7 +5,7 @@ import model.entities.Department;
 public class Program {
 
 	public static void main(String[] args) {
-
+		//DEPARTMENT TEST 
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
 
