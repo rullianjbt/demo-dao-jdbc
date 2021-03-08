2e60a528022f2dd7eabe71f156d7b0b34f6a162c
Rullian Siqueira da Mata <rullianjbt@gmail.com>
2021-03-08 13:35:05 -0300

    SellerDAOJDBC and DAOFactory

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Date;
 
 import model.entities.Sellert;
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Seller;
 
 public class Program {
@@ -12,6 +14,8 @@ public class Program {
 		Sellert obj = new Sellert(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDAO sellerDao = new DAOFactory().createSellerDAO();
 		
 		System.out.println(seller);
 
